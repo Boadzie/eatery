e3882f39221a2080836a010e8e4a47c4f1bfeeae
Daniel Boadzie <boadziedaniel43@gmail.com>
2021-08-12 16:29:53 +0000

    add menu section

---
+++ src/lib/Menu.svelte
@@ -0,0 +1,72 @@
+<section class="py-24 text-white bg-gray-100">
+    <h2 class="pb-8 text-5xl font-bold text-center text-gray-500">Special Menu</h2>
+	<div class="flex items-center justify-center">
+		<div class="grid grid-cols-1 mx-5 md:grid-cols-2 lg:grid-cols-4 lg:mx-14 gap-y-2">
+			<div class="flex flex-col justify-center bg-white">
+				<h2 class="p-2 font-mono text-3xl text-gray-500">Grilled Caesar salad</h2>
+				<p class="p-3 text-lg font-bold text-gray-400">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto illo delectus...
+				</p>
+				<span class="p-3 text-2xl font-extrabold tomato">$14.54</span>
+			</div>
+			<div>
+				<img
+					alt="food"
+					src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Zm9vZHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
+					class="object-cover object-center w-full h-64 overflow-hidden"
+				/>
+			</div>
+			<div class="flex flex-col justify-center bg-white ">
+				<h2 class="p-3 font-mono text-3xl text-gray-500">Golden Spagetti</h2>
+				<p class="p-3 text-lg font-bold text-gray-400">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto illo delectus...
+				</p>
+				<span class="p-3 text-2xl font-extrabold tomato">$19.99</span>
+			</div>
+			<div>
+				<img
+					alt="food"
+					src="https://images.unsplash.com/photo-1481931098730-318b6f776db0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=637&q=80"
+					class="object-cover object-center w-full h-64 overflow-hidden rounded-sm"
+				/>
+			</div>
+            <div>
+				<img
+					alt="food"
+					src="https://images.unsplash.com/photo-1548943487-a2e4e43b4853?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzB8fGZvb2R8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
+					class="object-cover object-center w-full h-64 overflow-hidden rounded-sm bg-middle"
+				/>
+			</div>
+            <div class="flex flex-col justify-center bg-white ">
+				<h2 class="p-3 font-mono text-3xl text-gray-500">Jangle Salad</h2>
+				<p class="p-3 text-lg font-bold text-gray-400">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto illo delectus...
+				</p>
+				<span class="p-3 text-2xl font-extrabold tomato">$43.00</span>
+			</div>
+			
+            <div>
+				<img
+					alt="food"
+					src="https://images.unsplash.com/photo-1460306855393-0410f61241c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODZ8fGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
+					class="object-cover object-center w-full h-64 overflow-hidden rounded-sm"
+				/>
+			</div>
+            <div class="flex flex-col justify-center bg-white ">
+				<h2 class="p-3 font-mono text-3xl text-gray-500">Jangle Chips</h2>
+				<p class="p-3 text-lg font-bold text-gray-400">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto illo delectus...
+				</p>
+				<span class="p-3 text-2xl font-extrabold tomato">$15.50</span>
+			</div>
+			
+		</div>
+	</div>
+</section>
+<style>
+	.tomato {
+		color: tomato;
+	}
+
+	
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,13 @@
 <script>
 	import Banner from '$lib/Banner.svelte';
 	import Feature from '$lib/Feature.svelte';
-import Special from '$lib/Special.svelte';
+    import Special from '$lib/Special.svelte';
 	import Swiper from '$lib/Swiper.svelte';
+	import Menu from '$lib/Menu.svelte';
 </script>
 
 <Banner />
 <Feature />
 <Special/>
+<Menu/>
 <Swiper/>
