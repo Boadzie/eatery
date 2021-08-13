473266a959478625fcc885f496e47bd5c99f75ef
Daniel Boadzie <boadziedaniel43@gmail.com>
2021-08-13 11:34:18 +0000

    add footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,13 +1,14 @@
 <script>
 	import '../app.postcss';
 	import Nav from '$lib/Nav.svelte';
+import Footer from '$lib/Footer.svelte';
 </script>
 
 <Nav />
 <main>
 	<slot />
 </main>
-
+<Footer/>
 <style>
 	:global(body) {
 		font-family: 'Nixie One', cursive;
